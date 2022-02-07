13172174cca0e24270d0937ac243a2129cc12a20
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-07 19:14:19 +0100

    Cluster module - Getting Starting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-
+const cluster = require("cluster");
 const totalCPUs = require('os').cpus().length;
-
-const app = require('./app');
-
 const env = require('./config');
 
 const main = () => {
-  app.listen(env.PORT);
-  console.log('Server listen on port', env.PORT);
-  console.log(`Total Number of CPU Counts is ${totalCPUs}`);
+
+  if (cluster.isMaster) {
+    console.log(`Total Number of CPU Counts is ${totalCPUs}`);
+    for (var i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+    }
+    // ONLINE Event
+    cluster.on("online", worker => {
+        console.log(`Worker Id = ${worker.id} - PID is ${worker.process.pid}`);
+    });
+    cluster.on("exit", worker => {
+        console.log(`Worker Id ${worker.id} - PID is ${worker.process.pid} is offline`);
+        console.log("Let's fork new worker!");
+        cluster.fork();
+    });
+  }
+  else {
+      const app = require('./app');
+      app.listen(env.PORT);
+      console.log(`(wpid: ${cluster.worker.process.pid}): Server listen on port ${env.PORT}`);
+  }
+
 };
+
 
 main();
--- src/routes/fibonacci.routes.js
+++ src/routes/fibonacci.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const wpid = require("cluster").worker.process.pid;
 const fabObj = require("../math-logic/fibonacci-series");
 
 router.get('/', (req, res) => {
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
 
 
 router.get("/calculate", (request, response) => {
+  console.log(`Worker Process Id ${wpid} has accepted the request!`);
   let number = fabObj.calculateFibonacciValue(Number.parseInt(request.query.number));
   response.send(`<h1>${number}</h1>`);
 });
