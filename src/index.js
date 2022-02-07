85ca87a25f10b8fd7b0d0aec4b30feb4dd79c6df
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-07 19:51:27 +0100

    Install and configure Pino logger

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const cluster = require("cluster");
 const totalCPUs = require('os').cpus().length;
 const env = require('./config');
+const logger = require('./logger');
 
 const main = () => {
 
   if (cluster.isMaster) {
-    console.log(`Total Number of CPU Counts is ${totalCPUs}`);
+    logger.info(`Total Number of CPU Counts is ${totalCPUs}`);
     for (var i = 0; i < totalCPUs; i++) {
         cluster.fork();
     }
     // ONLINE Event
     cluster.on("online", worker => {
-        console.log(`Worker Id = ${worker.id} - PID is ${worker.process.pid}`);
+        logger.info(`Worker Id = ${worker.id} - PID is ${worker.process.pid}`);
     });
     cluster.on("exit", worker => {
-        console.log(`Worker Id ${worker.id} - PID is ${worker.process.pid} is offline`);
-        console.log("Let's fork new worker!");
+        logger.info(`Worker Id ${worker.id} - PID is ${worker.process.pid} is offline`);
+        logger.info("Let's fork new worker!");
         cluster.fork();
     });
   }
   else {
       const app = require('./app');
       app.listen(env.PORT);
-      console.log(`(wpid: ${cluster.worker.process.pid}): Server listen on port ${env.PORT}`);
+      logger.info(`(wpid: ${cluster.worker.process.pid}): Server listen on port ${env.PORT}`);
   }
 
 };
--- src/routes/fibonacci.routes.js
+++ src/routes/fibonacci.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const wpid = require("cluster").worker.process.pid;
+const logger = require('../logger');
+const worker = require("cluster").worker;
 const fabObj = require("../math-logic/fibonacci-series");
 
 router.get('/', (req, res) => {
@@ -8,7 +9,7 @@ router.get('/', (req, res) => {
 
 
 router.get("/calculate", (request, response) => {
-  console.log(`Worker Process Id ${wpid} has accepted the request!`);
+  logger.info(`Worker ${worker.id} with process Id ${worker.process.pid} has accepted the request!`);
   let number = fabObj.calculateFibonacciValue(Number.parseInt(request.query.number));
   response.send(`<h1>${number}</h1>`);
 });
