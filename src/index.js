8ceceeab8b74b511f609c4be70e83ea8ca1e4f87
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2022-02-07 18:18:23 +0100

    Count total number of CPU cores

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+const totalCPUs = require('os').cpus().length;
+
 const app = require('./app');
 
 const env = require('./config');
@@ -5,6 +8,7 @@ const env = require('./config');
 const main = () => {
   app.listen(env.PORT);
   console.log('Server listen on port', env.PORT);
+  console.log(`Total Number of CPU Counts is ${totalCPUs}`);
 };
 
 main();
